26a320115a32ed2cf94a7cc484b13229afa674e8
LuisDamas <91203176+LuisDamas@users.noreply.github.com>
2023-11-20 23:39:43 +0000

    Delete src

--- src/main/java/model/NoPathException.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import com.brunomnsilva.smartgraph.graph.Vertex;
-
-public class NoPathException extends RuntimeException {
-    public NoPathException(String vertexId) {
-        super("There is no path to vertex " + vertexId);
-    }
-}
